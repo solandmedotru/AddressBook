bde2166925423deded03a1784281a7717a03969f
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-20 13:47:08 +0300

    Created all res files, layouts and menu

--- app/src/main/java/ru/solandme/addressbook/ContactsFragment.java
+++ app/src/main/java/ru/solandme/addressbook/ContactsFragment.java
@@ -1,7 +1,7 @@
 package ru.solandme.addressbook;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +14,6 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 }
