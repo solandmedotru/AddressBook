f3ef354b5d1514088d64e074a7cac462ba0a7fd8
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-21 12:48:17 +0300

    Created Database description class

--- app/src/main/java/ru/solandme/addressbook/data/DatabaseDescription.java
+++ app/src/main/java/ru/solandme/addressbook/data/DatabaseDescription.java
@@ -1,8 +1,29 @@
 package ru.solandme.addressbook.data;
 
-/**
- * Created by SolanD on 20.09.16.
- */
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
 
 public class DatabaseDescription {
+    public static final String AUTHORITY = "ru.solandme.addressbook.data";
+
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final class Contact implements BaseColumns {
+        public static final String TABLE_NAME = "contacts";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(TABLE_NAME).build();
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_STREET = "street";
+        public static final String COLUMN_CITY = "city";
+        public static final String COLUMN_STATE = "state";
+        public static final String COLUMN_ZIP = "zip";
+
+        public static Uri buildContactUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
 }
