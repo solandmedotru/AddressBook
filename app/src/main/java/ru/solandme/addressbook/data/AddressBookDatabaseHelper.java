df309335e038eccd5d07e7848ce7a1ffdf5bc2af
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-21 13:01:47 +0300

    Created Database helper

--- app/src/main/java/ru/solandme/addressbook/data/AddressBookDatabaseHelper.java
+++ app/src/main/java/ru/solandme/addressbook/data/AddressBookDatabaseHelper.java
@@ -4,14 +4,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import ru.solandme.addressbook.data.DatabaseDescription.Contact;
 
 public class AddressBookDatabaseHelper extends SQLiteOpenHelper {
-    public AddressBookDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DATABASE_NAME = "AddressBook.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public AddressBookDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        final String CREATE_CONTACTS_TABLE = "CREATE TABLE " +
+                Contact.TABLE_NAME + "(" +
+                Contact._ID + " integer primary key, " +
+                Contact.COLUMN_NAME + " TEXT, " +
+                Contact.COLUMN_PHONE + " TEXT, " +
+                Contact.COLUMN_EMAIL + " TEXT, " +
+                Contact.COLUMN_STREET + " TEXT, " +
+                Contact.COLUMN_CITY + " TEXT, " +
+                Contact.COLUMN_STATE + " TEXT, " +
+                Contact.COLUMN_ZIP + " TEXT);";
+        db.execSQL(CREATE_CONTACTS_TABLE);
 
     }
 
