4f5bcfcbdce777f2a7fe5e05b25bd818783787d9
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-21 13:37:35 +0300

    Created Content provider

--- app/src/main/java/ru/solandme/addressbook/data/AddressBookContentProvider.java
+++ app/src/main/java/ru/solandme/addressbook/data/AddressBookContentProvider.java
@@ -2,49 +2,136 @@ package ru.solandme.addressbook.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
+import ru.solandme.addressbook.R;
+import ru.solandme.addressbook.data.DatabaseDescription.Contact;
+
 public class AddressBookContentProvider extends ContentProvider {
+
+    private AddressBookDatabaseHelper dbHelper;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private static final int ONE_CONTACT = 1;
+    private static final int CONTACTS = 2;
+
+    static {
+        uriMatcher.addURI(DatabaseDescription.AUTHORITY, Contact.TABLE_NAME + "/#", ONE_CONTACT);
+        uriMatcher.addURI(DatabaseDescription.AUTHORITY, Contact.TABLE_NAME, CONTACTS);
+    }
+
     public AddressBookContentProvider() {
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int numberOfRowsDeleted;
+
+        switch (uriMatcher.match(uri)) {
+            case ONE_CONTACT:
+                String id = uri.getLastPathSegment();
+
+                numberOfRowsDeleted = dbHelper.getWritableDatabase().delete(
+                        Contact.TABLE_NAME, Contact._ID + "=" + id, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        getContext().getString(R.string.invalid_delete_uri) + uri);
+        }
+
+        if (numberOfRowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numberOfRowsDeleted;
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Uri newContactUri = null;
+
+        switch (uriMatcher.match(uri)) {
+            case CONTACTS:
+                long rowId = dbHelper.getWritableDatabase().insert(Contact.TABLE_NAME, null, values);
+
+                if (rowId > 0) {
+                    newContactUri = Contact.buildContactUri(rowId);
+                    getContext().getContentResolver().notifyChange(uri, null);
+                } else
+                    throw new SQLException(getContext().getString(R.string.insert_failed) + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        getContext().getString(R.string.invalid_insert_uri) + uri);
+        }
+
+        return newContactUri;
     }
 
     @Override
     public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
+        dbHelper = new AddressBookDatabaseHelper(getContext());
+        return true;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Cursor query(Uri uri, String[] projection,
+                        String selection, String[] selectionArgs, String sortOrder) {
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(Contact.TABLE_NAME);
+
+        switch (uriMatcher.match(uri)) {
+            case ONE_CONTACT:
+                queryBuilder.appendWhere(
+                        Contact._ID + "=" + uri.getLastPathSegment());
+                break;
+            case CONTACTS:
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        getContext().getString(R.string.invalid_query_uri) + uri);
+        }
+
+        Cursor cursor = queryBuilder.query(dbHelper.getReadableDatabase(),
+                projection, selection, selectionArgs, null, null, sortOrder);
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int update(Uri uri, ContentValues values,
+                      String selection, String[] selectionArgs) {
+        int numberOfRowsUpdated;
+        switch (uriMatcher.match(uri)) {
+            case ONE_CONTACT:
+                String id = uri.getLastPathSegment();
+                numberOfRowsUpdated = dbHelper.getWritableDatabase().update(
+                        Contact.TABLE_NAME, values, Contact._ID + "=" + id,
+                        selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        getContext().getString(R.string.invalid_update_uri) + uri);
+        }
+
+        if (numberOfRowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numberOfRowsUpdated;
     }
+
 }
